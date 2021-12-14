7e34c5f0f20d642b01ec0885a5ac1b2d3fe64bf1
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-14 12:51:37 +0000

    add event listener

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const smallCups = document.querySelectorAll(".cup-small");
 const litres = document.getElementById("litres");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => highlightCups(idx));
+});
