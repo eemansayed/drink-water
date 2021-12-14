055e5e143f2e9cfcd0b394317ed6f2f019b18de7
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-14 11:35:09 +0000

    add three consts

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 const smallCups = document.querySelectorAll(".cup-small");
+const litres = document.getElementById("litres");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
